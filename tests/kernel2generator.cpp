339aec0e64c7b385e2deb49b8bcc6384b446ac3c
myst6re <myst6re@gmail.com>
2016-06-19 23:21:58 +0200

    Adding dummy test file, referenced in the pro file

---
+++ tests/kernel2generator.cpp
@@ -0,0 +1,6 @@
+#include "kernel2generator.h"
+
+Kernel2Generator::Kernel2Generator()
+{
+	
+}
---
+++ tests/kernel2generator.h
@@ -0,0 +1,11 @@
+#ifndef KERNEL2GENERATOR_H
+#define KERNEL2GENERATOR_H
+
+
+class Kernel2Generator
+{
+public:
+	Kernel2Generator();
+};
+
+#endif // KERNEL2GENERATOR_H
